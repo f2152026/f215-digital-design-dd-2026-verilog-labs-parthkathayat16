336621d2bb414898b9fdcfa8b5f5b61c1a17c0a3
ParthKathayat16 <f20240963@pilani.bits-pilani.ac.in>
2026-09-19 17:04:48 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,11 +1,20 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Testbench to verify the parameterized lookup table (lut.v).
 
 module tb;
 
   // TODO: declare the inputs and outputs
+  // For default DEPTH=4, sel is 2 bits. For default WIDTH=8, dout is 8 bits.
+  reg  [1:0] t_sel;
+  wire [7:0] t_dout;
+
+  integer i;
 
   // TODO: instantiate DUT here
+  lut DUT (
+    .sel  (t_sel),
+    .dout (t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -18,10 +27,17 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
-
+    // Loop through all 4 addresses (0 to 3), waiting 5 time units between each
+    for (i = 0; i < 4; i = i + 1) begin
+      t_sel = i;
+      #5;
+    end
+    
+    $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    // change as required to match the LUT signals
+    $monitor($time, " sel=%0d | dout=%0d (which is %0d squared)", t_sel, t_dout, t_sel); 
 
 endmodule
